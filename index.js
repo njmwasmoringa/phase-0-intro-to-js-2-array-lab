48e9a04a7f9ff79545c7fe04136646c525d9dab9
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-02-08 15:07:41 +0300

    Completed Asignement

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+function destructivelyAppendCat(name){
+    let cats = ["Milo", "Otis", "Garfield"];
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    let cats = ["Milo", "Otis", "Garfield"];
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    let cats = ["Milo", "Otis", "Garfield"];
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    let cats = ["Milo", "Otis", "Garfield"];
+    cats.shift();
+}
+
+function appendCat(name){
+    let cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    let cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    let cats = ["Milo", "Otis", "Garfield"];
+    let newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat(){
+    let cats = ["Milo", "Otis", "Garfield"];
+    let newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}	
